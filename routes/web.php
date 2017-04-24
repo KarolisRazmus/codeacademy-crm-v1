be7b11615bd6373ce668e5bf74bf088d5ca60278
KarolisRazmus <karolisrazmus@gmail.com>
2017-04-24 03:02:01 +0300

    added clients route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,16 @@ Route::get('/persons',[
     'uses' => 'CRMPersonsController@index',
 ]);
 
-Route::get('/generate-fake-data/persons/{count}',[
+Route::get('/clients',[
+    'uses' => 'CRMClientsController@index',
+]);
+
+Route::get('/generate-fake-data/persons/{count?}',[
     'uses' => 'CRMFakeDataController@generatePersons',
+
+]);
+
+Route::get('/generate-fake-data/clients/{count?}',[
+    'uses' => 'CRMFakeDataController@generateClients',
 
 ]);
